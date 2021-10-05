5a3523f2b66b4407b6473cdf9ebddcdf00f6d1d7
miladpeagh <miladpeagh1999@gmail.com>
2021-10-05 18:35:07 +0330

    Add current month name

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-import {isWeekend} from "./helpers.js";
-import {getDayName} from "./helpers.js";
-import {getDaysInMonthUTC} from "./helpers.js";
+// import {isWeekend} from "./helpers.js";
+// import {getDayName} from "./helpers.js";
+// import {getDaysInMonthUTC} from "./helpers.js";
 
 const calendar = document.querySelector('#app-calendar');
-
+const monthName = document.querySelector('#month-name');
 let year = new Date().getFullYear();
 let month = new Date().getMonth();
+let monthLong = new Intl.DateTimeFormat("en-US", { month: "long" }).format(new Date(Date.UTC(year, month, 1)));
+
+const isWeekend = day => {
+	// 6 when it's saturday, 0 when it's sunday
+	return day % 7 === 6 || day % 7 === 0
+}
+
+const getDayName = day => {
+	const date = new Date(Date.UTC(2021, 9, day));
+
+	const dayName = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+
+	return dayName;
+}
+
+const getDaysInMonthUTC = (month, year) => {
+  let date = new Date(Date.UTC(year, month, 1));
+  let days = [];
+  while (date.getUTCMonth() === month) {
+    days.push(new Date(date).getDate());
+    date.setUTCDate(date.getUTCDate() + 1);
+  }
+  return days;
+}
 
 let days = getDaysInMonthUTC(month, year);
+
+
+monthName.insertAdjacentHTML("beforeend",`<h1>${monthLong}</h1>`);
 
 for (let day = 1; day <= days.length; day++){
 
