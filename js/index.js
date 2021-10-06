c9d59bdadc9ed2c6272c52529755eb00ce29ea4e
miladpeagh <miladpeagh1999@gmail.com>
2021-10-06 21:29:41 +0330

    use refactored methods

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// import {isWeekend} from "./helpers.js";
-// import {isToday} from "./helpers.js";
-// import {getDayName} from "./helpers.js";
-// import {getDaysInMonthUTC} from "./helpers.js";
+import {isWeekend} from "./helpers.js";
+import {isToday} from "./helpers.js";
+import {getDayName} from "./helpers.js";
+import {getDaysInMonthUTC} from "./helpers.js";
 
 const calendar = document.querySelector('#app-calendar');
 const monthName = document.querySelector('#month-name');
@@ -11,33 +11,7 @@ let year = new Date().getFullYear();
 let month = new Date().getMonth();
 let monthLong = new Intl.DateTimeFormat("en-US", { month: "long" }).format(new Date(Date.UTC(year, month, 1)));
 
-const isWeekend = day => {
-	// 6 when it's saturday, 0 when it's sunday
-	return day % 7 === 6 || day % 7 === 0
-}
 
-const isToday = day => {
-	let now = new Date().getDate();
-	return day === now;
-}
-
-const getDayName = day => {
-	const date = new Date(Date.UTC(2021, 9, day));
-
-	const dayName = new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-
-	return dayName;
-}
-
-const getDaysInMonthUTC = (month, year) => {
-  let date = new Date(Date.UTC(year, month, 1));
-  let days = [];
-  while (date.getUTCMonth() === month) {
-    days.push(new Date(date).getDate());
-    date.setUTCDate(date.getUTCDate() + 1);
-  }
-  return days;
-}
 
 let days = getDaysInMonthUTC(month, year);
 
